444f4f8c53949bca2300217aa82286a64d143778
TGMM <tgmm.1000@gmail.com>
2026-05-23 17:24:17 -0700

    Removed unused dependencies and formatted files

--- src/main/java/com/kaliumstudios/mekanismcustommachines/MekanismCustomMachinesClient.java
+++ src/main/java/com/kaliumstudios/mekanismcustommachines/MekanismCustomMachinesClient.java
@@ -12,13 +12,16 @@ import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
 // This class will not load on dedicated servers. Accessing client side code from here is safe.
 @Mod(value = MekanismCustomMachines.MODID, dist = Dist.CLIENT)
-// You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+// You can use EventBusSubscriber to automatically register all static methods
+// in the class annotated with @SubscribeEvent
 @EventBusSubscriber(modid = MekanismCustomMachines.MODID, value = Dist.CLIENT)
 public class MekanismCustomMachinesClient {
     public MekanismCustomMachinesClient(ModContainer container) {
         // Allows NeoForge to create a config screen for this mod's configs.
-        // The config screen is accessed by going to the Mods screen > clicking on your mod > clicking on config.
-        // Do not forget to add translations for your config options to the en_us.json file.
+        // The config screen is accessed by going to the Mods screen > clicking on your
+        // mod > clicking on config.
+        // Do not forget to add translations for your config options to the en_us.json
+        // file.
         container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
     }
 
